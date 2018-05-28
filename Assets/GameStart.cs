4872d6c146bd4f619fad4471b80e1d3f9c5f51fc
DESKTOP-HMKQ025\Denis <boogerman48@gmail.com>
2018-05-28 21:23:54 +0300

    Package added

---
+++ Assets/GameStart.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameStart : MonoBehaviour {
+	void Start() {
+        StartCoroutine(WaitForIt(8.0F));
+    }
+
+    private void Update()
+    {
+
+    }
+
+    void OnMouseDown()
+    {
+        SceneManager.LoadScene("Start");
+    }
+
+    IEnumerator WaitForIt(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        SceneManager.LoadScene("Start");
+    }
+}
